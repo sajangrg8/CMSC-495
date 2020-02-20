52f5d2a999499a225d1d8c75ea97fbe052c325a5
Bou <masterenlightenment09@gmail.com>
2020-02-20 22:06:36 +1000

    Added empty messages in added class field and re-aligned the buttons.

--- gpacalculator.js
+++ gpacalculator.js
@@ -37,6 +37,26 @@
 
     $(document).ready(function () {
         $("#add-more").click(function(e){
-           $(".insertionpoint").after("<label class='sr-only' for='inlineFormInputName2'>Name</label><input type='text' class='form-control mb-2 mr-sm-2' id='inlineFormInputName2' placeholder='Class Name'><select class='form-control'><option name='oneCred'>1 Credit</option><option name='twoCred'>2 Credits</option><option name='threeCred'>3 Credits</option><option name='fourCred'>4 Credits</option><option name='fiveCred'>5 Credits</option><option name='sixCred'>6 Credits</option></select><select class='form-control'><option name='A'>A</option><option name='Aminus'>A-</option><option name='Bplus'>B+</option><option name='B'>B</option><option name='Bminus'>B-</option><option name='Cplus'>C+</option><option name='C'>C</option><option name='Cminus'>C-</option><option name='Dplus'>D+</option><option name='D'>D</option><option name='Dminus'>D-</option><option name='F'>F</option></select>");
+           $(".insertionpoint").after("<label class='sr-only' for='inlineFormInputName2'>Name</label>
+           <input type='text' class='form-control mb-2 mr-sm-2' id='inlineFormInputName2' placeholder='Class Name'><select class='form-control'><option name='oneCred'>1 Credit</option>
+           <option name='twoCred'>2 Credits</option>
+           <option name='threeCred'>3 Credits</option>
+           <option name='fourCred'>4 Credits</option>
+           <option name='fiveCred'>5 Credits</option>
+           <option name='sixCred'>6 Credits</option>
+           </select>
+           <select class='form-control'><option name='A'>A</option>
+           <option name='Aminus'>A-</option>
+           <option name='Bplus'>B+</option>
+           <option name='B'>B</option>
+           <option name='Bminus'>B-</option>
+           <option name='Cplus'>C+</option>
+           <option name='C'>C</option>
+           <option name='Cminus'>C-</option>
+           <option name='Dplus'>D+</option>
+           <option name='D'>D</option>
+           <option name='Dminus'>D-</option>
+           <option name='F'>F</option>
+           </select>");
         });
     });
